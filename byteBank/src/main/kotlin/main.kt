78a67acfa0f0492ff0a96ae74e810a99e7e3cf83
lucas-26 <luquinhas.araujo26@hotmail.com>
2021-03-04 09:34:54 -0300

    primeiro curso terminado

--- byteBank/src/main/kotlin/main.kt
+++ byteBank/src/main/kotlin/main.kt
@@ -1,27 +1,23 @@
 fun main() {
     println("Bem vindo ao byte bank")
 
-    val contalUCAS: Conta = Conta()
-    val contaFran: Conta = Conta()
+    val contalUCAS: Conta = Conta("Lucas", 2, 100.0)
+    val contaCarlos: Conta = Conta(saldo = 100.0, numero =  2, titular = "Lucas", )//labels, você passa os paramentros de maneira explicita e assim você não precisa passar na ordem que o construtor exige mas no final ele será passado de maneira correta
+    val contaFran: Conta = Conta("Francine", 1, 2000.0)
 
-
-
-    contalUCAS.titular = "Lucas"
-    contalUCAS.depositar(1000.0)
     contalUCAS.depositar(50.0)
     println(contalUCAS.titular)
     println("Sacando na conta do lucas")
     contalUCAS.sacar(400.0)
-    println(contalUCAS.getSaldo())
+    println(contalUCAS.saldo)
 
-
-    contaFran.titular = "Francine"
-    contaFran.depositar(2000.0)
     contaFran.depositar(70.0)
     println(contaFran.titular)
     println("Sacando na conta do Francine")
     contaFran.sacar(400.0)
-    println(contaFran.getSaldo())
+    println(contaFran.saldo)
+
+
     if (contalUCAS.tranferencia(contaFran, 55.0)) {
         println("Transfarencia para a fran efetuada");
     } else {
@@ -29,5 +25,5 @@ fun main() {
     }
 
     println("Sacando na conta do Francine");
-    println(contaFran.getSaldo());
+    println(contaFran.saldo);
 }
